d088ec011affc97a84104c33eb6e43bed135eb1d
Ucheduk <dukweb4@gmail.com>
2020-05-19 00:54:41 +0100

    feat: add dark mode

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,43 @@
 import { Link } from "gatsby";
-import React from "react";
+import React, { useEffect, useState} from "react";
 const logo = require('../assets/img/logo.png');
 
-class Header extends React.Component {
+const Header = () => {
 
-  openMobileNav = () => {
+  const [mode, setMode] = useState(localStorage.getItem('mode'));
+  const checkMode = () => {
+    if (mode === 'dark') {
+      const root = document.querySelector(':root');
+      const toggle = document.querySelector('.toggle');
+      toggle.classList.add("dark-mode");
+      root.style.setProperty('--bg-color', 'rgb(22, 7, 39)');
+      root.style.setProperty('--font-color', '#fff');
+      root.style.setProperty('--box-shadow', 'rgba(255,255,255,0.35)');
+    }
+  }
+  useEffect(() => checkMode(), [])
+
+  const toggleMode = () => {
+    const root = document.querySelector(':root');
+    const toggle = document.querySelector('.toggle');
+    // const bgColor = root.style.getPropertyValue('--bg-color')
+    if (mode === 'dark') {
+      root.style.setProperty('--bg-color', '#fff');
+      root.style.setProperty('--font-color', '#030805');
+      root.style.setProperty('--box-shadow', 'rgba(0,0,0,0.35)');
+      localStorage.setItem('mode', 'light');
+      setMode('light')
+    } else {
+      root.style.setProperty('--bg-color', 'rgb(22, 7, 39)');
+      root.style.setProperty('--font-color', '#fff');
+      root.style.setProperty('--box-shadow', 'rgba(255,255,255,0.35)');
+      localStorage.setItem('mode', 'dark');
+      setMode('dark')
+    }
+    toggle.classList.toggle("dark-mode");
+  }
+
+  const openMobileNav = () => {
     let mainHeader = document.querySelector(".main-header");
     let mobileNav = document.querySelector(".mobile-nav");
     let menu = document.querySelector(".menu");
@@ -15,7 +48,7 @@ class Header extends React.Component {
     if(close) close.classList.add("display");
   }
 
-  closeMobileNav = () => {
+  const closeMobileNav = () => {
     let mainHeader = document.querySelector(".main-header");
     let mobileNav = document.querySelector(".mobile-nav");
     let menu = document.querySelector(".menu");
@@ -26,35 +59,40 @@ class Header extends React.Component {
     if(close) close.classList.remove("display");
   }
 
-  render() {
-    return (
-      <header className="main-header">
-        <div className="container main-header-row">
-          <Link to="/"><img className="main-header-logo" src={logo} alt="My Logo" /></Link>
-          <nav className="main-header-nav">
-            <ul>
-              <li><Link to="/" className="underline-from-left" activeClassName="active">Home</Link></li>
-              <li><Link to="/about" className="underline-from-left" activeClassName="active">About</Link></li>
-              <li><Link to="/portfolio" className="underline-from-left" activeClassName="active">Portfolio</Link></li>
-              <li><Link to="/blog" className="underline-from-left" activeClassName="active">My Blog</Link></li>
-              <li><Link to="/contact" className="underline-from-left" activeClassName="active">Contact</Link></li>
-            </ul>
-          </nav>
-          <i onClick={this.openMobileNav} className="zmdi zmdi-menu menu"></i>
-          <i onClick={this.closeMobileNav} className="zmdi zmdi-close close"></i>
-        </div>
-        <nav className="mobile-nav">
+  return (
+    <header className="main-header">
+      <div className="container main-header-row">
+        <Link to="/"><img className="main-header-logo" src={logo} alt="My Logo" /></Link>
+        <nav className="main-header-nav">
           <ul>
-            <li><Link onClick={this.closeMobileNav} to="/" className="underline-from-left" activeClassName="active">Home</Link></li>
-            <li><Link onClick={this.closeMobileNav} to="/about" className="underline-from-left" activeClassName="active">About</Link></li>
-            <li><Link onClick={this.closeMobileNav} to="/portfolio" className="underline-from-left" activeClassName="active">Portfolio</Link></li>
-            <li><Link onClick={this.closeMobileNav} to="/blog" className="underline-from-left" activeClassName="active">My Blog</Link></li>
-            <li><Link onClick={this.closeMobileNav} to="/contact" className="underline-from-left" activeClassName="active">Contact</Link></li>
+            <li><Link to="/" className="underline-from-left" activeClassName="active">Home</Link></li>
+            <li><Link to="/about" className="underline-from-left" activeClassName="active">About</Link></li>
+            <li><Link to="/portfolio" className="underline-from-left" activeClassName="active">Portfolio</Link></li>
+            <li><Link to="/blog" className="underline-from-left" activeClassName="active">My Blog</Link></li>
+            <li><Link to="/contact" className="underline-from-left" activeClassName="active">Contact</Link></li>
           </ul>
-        </nav> 
-      </header>
-    )
-  }
+          
+        </nav>
+          
+        <span onClick={openMobileNav} className="menu">
+          <i className="zmdi zmdi-menu "></i>
+        </span>
+        <i onClick={closeMobileNav} className="zmdi zmdi-close close"></i>
+        <div className="toggle-wrapp">
+            <span className="toggle" onClick={toggleMode}></span>
+          </div>
+      </div>
+      <nav className="mobile-nav">
+        <ul>
+          <li><Link onClick={closeMobileNav} to="/" className="underline-from-left" activeClassName="active">Home</Link></li>
+          <li><Link onClick={closeMobileNav} to="/about" className="underline-from-left" activeClassName="active">About</Link></li>
+          <li><Link onClick={closeMobileNav} to="/portfolio" className="underline-from-left" activeClassName="active">Portfolio</Link></li>
+          <li><Link onClick={closeMobileNav} to="/blog" className="underline-from-left" activeClassName="active">My Blog</Link></li>
+          <li><Link onClick={closeMobileNav} to="/contact" className="underline-from-left" activeClassName="active">Contact</Link></li>
+        </ul>
+      </nav> 
+    </header>
+  )
 }
 
 export default Header
